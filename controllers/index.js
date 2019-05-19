7bb934f7d426779d47c5217510958b8737ea5d37
doğukan deniz <dogukan.deniz@stop63.com>
2019-05-20 00:14:42 +0300

    ilk commit bcrypt removde

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,16 @@
 const User = require('../models/Users')
 const jwt = require('jsonwebtoken')
-const multer  = require('multer');
+const multer = require('multer');
 const upload = multer()
 
 let configTop
 module.exports = function (mongoose) {
 
-    
+
     return {
-        SetRouting: function (router,config) {
-            configTop=config
-          
+        SetRouting: function (router, config) {
+            configTop = config
+
             router.post('/user/register', this.registerUser);
             router.post('/user/authenticate', this.loginUser);
             router.post('/api/user/imageUpload', this.userImageUpload);
@@ -18,83 +18,83 @@ module.exports = function (mongoose) {
         },
         registerUser: function (req, res, next) {
 
-            const {username,password,email,enrollType} = req.body
+            const { username, password, email, enrollType } = req.body
 
-     
-                const user = new User({username,password,email,enrollType})
-                const promise =user.save()
-    
-                promise.then((data)=> {
-                    res.json(data)
-                }).catch((err)=> {
-                    res.json({status:false,error:err})
-                })
+
+            const user = new User({ username, password, email, enrollType })
+            const promise = user.save()
+
+            promise.then((data) => {
+                res.json(data)
+            }).catch((err) => {
+                res.json({ status: false, error: err })
             })
-           
-        },
       
-        loginUser:function(req,res,next){
 
-            const {username,password} =  req.body
+    },
+
+        loginUser: function(req, res, next) {
+
+            const { username, password } = req.body
 
             User.findOne({
                 username,
                 password
-            },(err,user)=> {
-                if(err)
+            }, (err, user) => {
+                if (err)
                     throw err
-                if(!user){
+                if (!user) {
                     res.json({
                         status: false,
-                        message:'Authentication failed,user not found'
+                        message: 'Authentication failed,user not found'
                     })
                 }
-
-                   
-                    else{
-                            const payload = {
-                                user
-                            }
-                            const token = jwt.sign(payload,configTop.api_secret_key,{expiresIn:720
-                            })
-
-                            res.json({
-                                status:true,
-                                token
-                            })
-                        }
-                   
-            })
-        },
-        userImageUpload:function(req,res,next){
-            
-            const token = req.query.token || req.headers['x-access-token'] || req.body.token 
-            const storage = multer.diskStorage({
-                destination: function (req, file, cb) {
-                  cb(null, './uploads')
-                },
-                filename: function (req, file, cb) {
-                      const extArray = file.mimetype.split("/");
-                      const extension = extArray[extArray.length - 1];
-              
-                  cb(null, Date.now() + '.' + extension)
-                }
-              });
-          
-          
-            console.log(req.file)
-
-            const user = User.findByIdAndUpdate(req.decodde.user._id,{userImage:req.file.filename},(err,data)=> {
-                    if(err){
-                        console.log(err)
-                    }else{
-                        res.json({status:1})
+                else {
+                    const payload = {
+                        user
                     }
-            })
-            
-            
+                    const token = jwt.sign(payload, configTop.api_secret_key, {
+                        expiresIn: 720
+                    })
 
+                    res.json({
+                        status: true,
+                        token
+                    })
+
+                }
+                })
         },
-       
-    }
+    
+    userImageUpload: function(req, res, next) {
+
+        const token = req.query.token || req.headers['x-access-token'] || req.body.token
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, './uploads')
+            },
+            filename: function (req, file, cb) {
+                const extArray = file.mimetype.split("/");
+                const extension = extArray[extArray.length - 1];
+
+                cb(null, Date.now() + '.' + extension)
+            }
+        });
+
+
+        console.log(req.file)
+
+        const user = User.findByIdAndUpdate(req.decodde.user._id, { userImage: req.file.filename }, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json({ status: 1 })
+            }
+        })
+
+
+
+    },
+
+}
 }
